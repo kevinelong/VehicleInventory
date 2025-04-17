cae2ca910e8b45b3c0f2fcc3227148f7acbfde58
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-17 14:53:23 -0700

    update2

--- src/Main.java
+++ src/Main.java
@@ -1,93 +1,74 @@
 import java.util.Scanner;
 
-class Vehicle {
-    //instance variable AKA properties
-    private long vehicleId;//    vehicleId - a long (ex: 101121)
-    private String makeModel;    //    makeModel - a string (ex: Ford Explorer)
-    private String color;    //    color - a string (ex: Red)
-    private long odometerReading;    //    odometerReading - an int (ex: 32775)
-    private double price;    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
-
-    //Constructor
-    Vehicle(
-            long vehicleId,//    vehicleId - a long (ex: 101121)
-            String makeModel,    //    makeModel - a string (ex: Ford Explorer)
-            String color,    //    color - a string (ex: Red)
-            long odometerReading,    //    odometerReading - an int (ex: 32775)
-            double price    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
-    ) {
-        this.vehicleId = vehicleId; //    vehicleId - a long (ex: 101121)
-        this.makeModel = makeModel;    //    makeModel - a string (ex: Ford Explorer)
-        this.color = color;    //    color - a string (ex: Red)
-        this.odometerReading = odometerReading;    //    odometerReading - an int (ex: 32775)
-        this.price = price;    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
+public class Main {
+    public static void listAllVehicles(Vehicle[] vehicles) {
+        System.out.println("1. List All Vehicles:");
+        for (int i = 0; i < vehicles.length; i++) {
+            System.out.printf("%d: %s\n", i, vehicles[i]);
+        }
     }
 
-    //GETTERS
-    public long getVehicleId(){
-        return this.vehicleId;
-    }//    vehicleId - a long (ex: 101121)
-    public String getMakeModel(){
-        return this.makeModel;
-    }    //    makeModel - a string (ex: Ford Explorer)
-    public String getColor(){
-        return this.color;
-    }    //    color - a string (ex: Red)
-    public long getOdometerReading(){
-        return this.odometerReading;
-    }    //    odometerReading - an int (ex: 32775)
-    public double getPrice(){
-        return this.price;
-    }    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
-    //SETTERS
-    public void setVehicleId(long vehicleId){
-        this.vehicleId = vehicleId;
-    }//    vehicleId - a long (ex: 101121)
-    public void setMakeModel(String makeModel){
-        this.makeModel = makeModel;
-    }    //    makeModel - a string (ex: Ford Explorer)
-    public void setColor(String color) {
-        this.color = color;
-    }    //    color - a string (ex: Red)
-    public void setOdometerReading(long odometerReading){
-        this.odometerReading = odometerReading;
-    }    //    odometerReading - an int (ex: 32775)
-    public void setPrice(double price){
-        this.price = price;
-    };    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
-}
+    public static void searchByMakeOrModel(Vehicle[] vehicles) {
+        System.out.println("2. Search by Make/Model:");
+    }
 
-public class Main {
+    public static void searchByPriceRange(Vehicle[] vehicles) {
+        System.out.println("3. Search by Price Range:");
+    }
+
+    public static void searchByColor(Vehicle[] vehicles) {
+        System.out.println("4. Search by Color:");
+    }
+
+    public static void addVehicle(Vehicle[] vehicles, int counter) {
+        System.out.println("5. Add a Vehicle:");
+        Vehicle v = new Vehicle(0,"Neo", "Black",0, -1);
+        vehicles[counter] = v;
+    }
+
     public static void main(String[] args) {
-        Vehicle[] vehicles = new Vehicle[]{
-            new Vehicle(101121,"Ford Explorer","Red",45000,13500),
-                new Vehicle(101122,"Toyota Camry","Blue",60000,11000),
-                new Vehicle(101123,"Chevrolet Malibu","Black",50000,9700),
-                new Vehicle(101124,"Honda Civic","White",70000,7500),
-                new Vehicle(101125,"Subaru Outback","Green",55000,14500),
-                new Vehicle(101126,"Jeep Wrangler","Yellow",30000,16000)
-        };
-        int counter = vehicles.length;
+
+        int counter = 0;
+        Vehicle[] vehicles = new Vehicle[20];
+
+        vehicles[counter++] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
+        vehicles[counter++] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
+        vehicles[counter++] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
+        vehicles[counter++] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
+        vehicles[counter++] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
+        vehicles[counter++] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
 
         //heredoc style quotes in newer java
         String prompt = """
-                What do you want to do?
-                1 - List all vehicles
-                2 - Search by make/model
-                3 - Search by price range
-                4 - Search by color
-                5 - Add a vehicle
-                6 - Quit
-                Enter your command:
-        """;
+                        What do you want to do?
+                        1 - List all vehicles
+                        2 - Search by make/model
+                        3 - Search by price range
+                        4 - Search by color
+                        5 - Add a vehicle
+                        6 - Quit
+                        Enter your command:
+                """;
 
         Scanner in = new Scanner(System.in);
         int command = 0;
 
         //Near infinite loop?
-        while(command != 6){
+        while (command != 6) {
             System.out.println(prompt);
             command = in.nextInt();
+            switch (command) {
+                case 1:
+                    Main.listAllVehicles(vehicles);
+                    break;
+                case 2:
+                    Main.searchByMakeOrModel(vehicles);
+                    break;
+                case 5:
+                    Main.addVehicle(vehicles, counter++);
+                default:
+                    System.out.println("NO SUCH COMMAND AS: " + command);
+            }
         }
 
         System.out.println("Bye Bye!!!");
---
+++ src/Vehicle.java
@@ -0,0 +1,65 @@
+public class Vehicle {
+    //instance variable AKA properties AKA member variable
+    private long vehicleId;//    vehicleId - a long (ex: 101121)
+    private String makeModel;    //    makeModel - a string (ex: Ford Explorer)
+    private String color;    //    color - a string (ex: Red)
+    private long odometerReading;    //    odometerReading - an int (ex: 32775)
+    private double price;    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
+
+    //Constructor
+    Vehicle(
+            long vehicleId,//    vehicleId - a long (ex: 101121)
+            String makeModel,    //    makeModel - a string (ex: Ford Explorer)
+            String color,    //    color - a string (ex: Red)
+            long odometerReading,    //    odometerReading - an int (ex: 32775)
+            double price    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
+    ) {
+        this.vehicleId = vehicleId; //    vehicleId - a long (ex: 101121)
+        this.makeModel = makeModel;    //    makeModel - a string (ex: Ford Explorer)
+        this.color = color;    //    color - a string (ex: Red)
+        this.odometerReading = odometerReading;    //    odometerReading - an int (ex: 32775)
+        this.price = price;    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
+    }
+
+    public String toString(){
+        return String.format("%d %s %s %d %.2f",
+                this.vehicleId,
+                this.makeModel,
+                this.color,
+                this.odometerReading,
+                this.price
+        );
+    }
+    //GETTERS
+    public long getVehicleId(){
+        return this.vehicleId;
+    }//    vehicleId - a long (ex: 101121)
+    public String getMakeModel(){
+        return this.makeModel;
+    }    //    makeModel - a string (ex: Ford Explorer)
+    public String getColor(){
+        return this.color;
+    }    //    color - a string (ex: Red)
+    public long getOdometerReading(){
+        return this.odometerReading;
+    }    //    odometerReading - an int (ex: 32775)
+    public double getPrice(){
+        return this.price;
+    }    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
+    //SETTERS
+    public void setVehicleId(long vehicleId){
+        this.vehicleId = vehicleId;
+    }//    vehicleId - a long (ex: 101121)
+    public void setMakeModel(String makeModel){
+        this.makeModel = makeModel;
+    }    //    makeModel - a string (ex: Ford Explorer)
+    public void setColor(String color) {
+        this.color = color;
+    }    //    color - a string (ex: Red)
+    public void setOdometerReading(long odometerReading){
+        this.odometerReading = odometerReading;
+    }    //    odometerReading - an int (ex: 32775)
+    public void setPrice(double price){
+        this.price = price;
+    };    //    price - a float (ex: 12250.00) (no Lamborghinis here!)
+}
